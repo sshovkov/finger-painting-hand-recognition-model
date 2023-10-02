81c89fd5f45561761b3ee373549ec6f7369196f7
sshovkov <sophiashovkovy@gmail.com>
2023-10-02 10:46:48 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Finger Painting with Hand Tracking
+
+Create digital paintings using your hand movements captured by a webcam. By using the MediaPipe library for hand tracking.
+
+## Prerequisites
+
+- Python 3.x
+- OpenCV (`pip install opencv-python`)
+- NumPy (`pip install numpy`)
+- MediaPipe (`pip install mediapipe`)
+
+## How to Run
+
+1. Clone this repository to your local machine.
+2. Make sure you have Python and the required libraries installed.
+3. Run the following command in your terminal or command prompt to start the finger painting application:
+
+   ```bash
+   python finger_painting.py
+   ```
+
+## Controls
+
+- **Right Hand**: Use your right index finger to draw on the screen.
+- **Left Hand**: Raise and hold your left hand to stop drawing.
+
+## Acknowledgements
+
+This project uses the [MediaPipe](https://mediapipe.dev/) library for hand tracking.
---
+++ finger_painting.py
@@ -0,0 +1,83 @@
+import cv2
+import numpy as np
+import mediapipe as mp
+import time
+
+# Initialize a hand tracking model using Mediapipe
+# https://developers.google.com/mediapipe/solutions/vision/hand_landmarker/python
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(
+    static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5
+)
+
+# Drawing variables
+drawing_color = (0, 255, 0)
+line_thickness = 10
+
+# Yse OpenCV for video
+cap = cv2.VideoCapture(0)
+
+# Get the width and height of the video frame
+frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+# Create canvas with the same dimensions as the video frame
+canvas = np.zeros((frame_height, frame_width, 3), dtype=np.uint8)
+
+# Previous point for smoothing
+prev_x, prev_y = 0, 0
+
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    # Flip the frame horizontally for selfie-view
+    frame = cv2.flip(frame, 1)
+
+    # Convert frame to RGB
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # Detect hand landmarks
+    results = hands.process(rgb_frame)
+
+    # If a hand is detected
+    if results.multi_hand_landmarks:
+        if results.multi_handedness[0].classification[0].label == "Left":
+            drawing_color = (0, 255, 0, 0)  # transparent
+            prev_x, prev_y = 0, 0  # Reset
+        else:
+            drawing_color = (0, 255, 0, 100)  # opaque
+
+            hand_landmarks = results.multi_hand_landmarks[0]
+            index_finger_tip = hand_landmarks.landmark[8]
+
+            x, y = int(index_finger_tip.x * frame_width), int(
+                index_finger_tip.y * frame_height
+            )
+
+            if prev_x == 0 and prev_y == 0:
+                prev_x, prev_y = x, y
+
+            # Smooth line between previous and current point (interpolation factor 0.1)
+            for i in range(1, 10):
+                cur_x = int(prev_x + (x - prev_x) * 0.1 * i)
+                cur_y = int(prev_y + (y - prev_y) * 0.1 * i)
+
+                cv2.circle(
+                    canvas, (cur_x, cur_y), line_thickness, drawing_color, -1
+                )  # -1 means filled circle
+
+            prev_x, prev_y = x, y
+
+    # Combine frame (100% opaque) and canvas (80% opaque)
+    result_frame = cv2.addWeighted(frame, 1, canvas, 0.8, 0)
+
+    # Display the resulting frame
+    cv2.imshow("Finger Painting", result_frame)
+
+    # Exit if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+# Cleanup
+cap.release()
+cv2.destroyAllWindows()
